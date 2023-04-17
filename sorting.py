a2ba698739eca8d8ea310aa76fd46359f8d06be3
Kristyna Bainarova <bainar.kr@gmail.com>
2023-04-17 09:55:14 +0200

    Zkouška444

--- sorting.py
+++ sorting.py
@@ -21,10 +21,29 @@ def read_data(file_name):
                     data[header].append(int(value))
     return data
 
+
+def selection_sort(num_array):
+    n = len(num_array)
+
+    for i in range(n):
+        min_idx = i
+        for num_idx in range(i + 1, n):
+            if num_array[num_idx] < num_array[min_idx]:
+                min_idx = num_idx
+        num_array[i], num_array[min_idx] = num_array[min_idx], num_array[i]
+
+    return num_array
+
+
+
 def main():
     my_data = read_data("numbers.csv")
-    print(my_data["series_3"])
-    print("Ahoj")
+    print(my_data["series_1"])
+    selection_sort_result = selection_sort(my_data["series_1"])
+    print(selection_sort_result[])
+    bubble_sort_result = bubble_sort(my_data["series_1"])
+    print(bubble_sort_result[])
+
 
 
 if __name__ == '__main__':
